90ea49d86c63cc5eee6173cde0b8e7ae505addec
Yash <yash9570raj@gmail.com>
2024-08-20 22:08:25 +0530

    add new features

--- src/components/TotalScore.jsx
+++ src/components/TotalScore.jsx
@@ -14,8 +14,11 @@ const TotalScore = () => {
 export default TotalScore;
 
 const ScoreContainer = styled.div`
+  max-width: 100px;
+  text-align: center;
   h1 {
     font-size: 100px;
+    line-height: 100px;
   }
   p {
     font-size: 24px;
