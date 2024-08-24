85561d808a704f61a3beee4e1439b408feac7183
Yash <yash9570raj@gmail.com>
2024-08-24 21:24:40 +0530

    new features

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import TotalScore from "./TotalScore";
 import NumberSelector from "./NumberSelector";
+import { styled } from "styled-components";
 
 const GamePlay = () => {
   return (
     <main>
-      <TotalScore />
-      <NumberSelector />
+      <div className="top_section">
+        <TotalScore />
+        <NumberSelector />
+      </div>
     </main>
   );
 };
 
 export default GamePlay;
+
+const MainContainer = styled.main`
+  .top_section {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+`;
